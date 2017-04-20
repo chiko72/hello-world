ad31ff1ecdf4f00536cd0ebe5b86c516c125b810
cs <csayouri@hotmail.fr>
2017-04-21 00:01:52 +0200

    deuxieme classe

---
+++ src/TestClass2.java
@@ -0,0 +1,12 @@
+
+
+
+public class TestClass2 {
+	public TestClass2() {
+		// TODO Auto-generated constructor stub
+	}
+  public static void main(String[] args) {
+	System.out.println("autre classe");
+}
+
+}
