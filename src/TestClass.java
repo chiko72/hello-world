91f80df49bbac7f37592c374a93ebd32e932b21e
cs <csayouri@hotmail.fr>
2017-04-20 23:35:40 +0200

    commite mas

--- src/TestClass.java
+++ src/TestClass.java
@@ -6,11 +6,16 @@ public class TestClass {
 		System.out.println("GIT");
 		System.out.println("SVN"); 
 		System.out.println("modif entre temps sur master");
-
+        //ajout entre temps2
+		System.out.println("ajout entre temps2");
 		System.out.println("modif baranche2");
 		
 		System.out.println("modif 2 branche2");
 		System.out.println("modif 3 branche 2");
+		System.out.println("aprèe merge");
+		
+		System.out.println("-----");
+		System.out.println("**********");
 
 	}
 
