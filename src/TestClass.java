7148701150f4c02bf46915c9a66f67f8ac062222
cs <csayouri@hotmail.fr>
2017-04-20 23:30:18 +0200

    synchro master branche2

--- src/TestClass.java
+++ src/TestClass.java
@@ -4,12 +4,14 @@ public class TestClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("GIT");
-		System.out.println("SVN");
-		
+		System.out.println("SVN"); 
+		System.out.println("modif entre temps sur master");
+
 		System.out.println("modif baranche2");
 		
 		System.out.println("modif 2 branche2");
 		System.out.println("modif 3 branche 2");
+
 	}
 
 }
