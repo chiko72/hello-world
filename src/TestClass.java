cd22d9a3577ae68407773fb8da5811bc725310ab
chakib <chakib@DESKTOP-MK0BI03>
2018-07-07 20:25:02 +0200

    ttt

--- src/TestClass.java
+++ src/TestClass.java
@@ -1,51 +1,7 @@
 public class TestClass {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("GIT");
-		System.out.println("SVN"); 
-		System.out.println("modif entre temps sur master");
-        //ajout entre temps2
-		System.out.println("ajout entre temps2");
-		System.out.println("modif baranche2");
-		
-		System.out.println("modif 2 branche2");
-		System.out.println("modif 3 branche 2");
-
-		System.out.println("aprèe merge");
-		
-		System.out.println("-----");
-		System.out.println("**********");
-
-		
-		System.out.println("modif 4 branche2");
-		System.out.println("modif 5 branche 2");
-		
-
-		System.out.println("modif sans pull");
-
-		//Modif sur le serveur
-//>>>>>>> branch 'master' of https://github.com/chiko72/hello-world.git
-        
-		System.out.println("modif dans classe 1 , alors que branche2 modifie classe2");
-        System.out.println("pour tager");
-        System.out.println("pour tager2");
-        
-        System.out.println("FSDFSDFS");
-        // Ajout Ali git 1
-        System.out.println("Ajout Ali git1");
-        //Ali
-        System.out.println("****");//
-        //--------------
-        System.out.println("ok");
-        //alo
-        //allo
-        //Ali commit alors qu'il y a eu entre temps
-        //Un commit de Mohamed sur ce fichier
-        //ajout Larbi git 3
-        //Ajout hmida
-        //----------------
-        //Ajout khammari
+System.out.println("debut*****************");
 	}
 
 }
